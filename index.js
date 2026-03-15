e6b391c4f3c4a188380f53f4559654dccc6f03e2
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-16 02:22:26 +0600

    fetched the api & designed the lesson buttons

---
+++ index.js
@@ -0,0 +1,19 @@
+const loadlessons = ()=>{
+    fetch("https://openapi.programming-hero.com/api/levels/all").then(res => res.json()).then(json => displayLessons(json.data))
+}
+
+const displayLessons = (lessons) => {
+    const levelContainer = document.getElementById('level-container')
+    levelContainer.innerHTML = "";
+    lessons.forEach((lesson) =>{
+        const btnDiv = document.createElement('div')
+        btnDiv.innerHTML = `
+            <button class="btn btn-outline btn-primary"> <i class="fa-solid fa-book-open"></i> Leason - ${lesson.level_no}</button>
+        
+        `
+        levelContainer.appendChild(btnDiv)
+    })
+}
+
+
+loadlessons()
